550e33d7917b968b2abe67a8c4046f3de07fc52e
Alberto Minetti <alberto.minetti@gmail.com>
2020-12-06 12:13:34 +0100

    refactoring for schedulers

--- backend/src/main/java/it/minetti/images/r/CovidGraphsGenerator.java
+++ backend/src/main/java/it/minetti/images/r/CovidGraphsGenerator.java
@@ -3,19 +3,14 @@ package it.minetti.images.r;
 import com.github.rcaller.rstuff.RCaller;
 import com.github.rcaller.rstuff.RCallerOptions;
 import com.github.rcaller.rstuff.RCode;
-import it.minetti.images.LocalGraphsService;
-import it.minetti.pcmdpc.RemoteCsvExtractor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.time.LocalDate;
-import java.util.Optional;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.apache.commons.io.IOUtils.resourceToString;
@@ -27,42 +22,6 @@ public class CovidGraphsGenerator {
     public static final String PLOT_NATIONAL = "/plot_national.R";
     public static final String PLOT_REGIONS = "/plot_regions.R";
 
-    private final LocalGraphsService localGraphsService;
-    private final RemoteCsvExtractor remoteCsvExtractor;
-
-    public CovidGraphsGenerator(LocalGraphsService localGraphsService, RemoteCsvExtractor remoteCsvExtractor) {
-        this.localGraphsService = localGraphsService;
-        this.remoteCsvExtractor = remoteCsvExtractor;
-    }
-
-    @Scheduled(cron = "0 55 17 * * ?", zone = "Europe/Rome")
-    @Scheduled(cron = "0 00 18 * * ?", zone = "Europe/Rome")
-    @Scheduled(cron = "0 10 18 * * ?", zone = "Europe/Rome")
-    @Scheduled(cron = "0 15 19 * * ?", zone = "Europe/Rome")
-    public GraphsGenerationResult runIfNewData() throws IOException {
-        Optional<LocalDate> optionalLastDayOfCalculation = localGraphsService.retrieveLatestCalculatedDay();
-        if (optionalLastDayOfCalculation.isEmpty()) {
-            log.warn("No graph has ever been calculated since now, seems it is the first time.");
-            return createLatestGraphs();
-        } else {
-            LocalDate lastDayOfCalculation = optionalLastDayOfCalculation.get();
-            if (lastDayOfCalculation.isEqual(LocalDate.now())) {
-                // the data for the day are published within the day
-                log.warn("No needs to create the new graphs since they are updated to {}.", lastDayOfCalculation);
-                return new GraphsGenerationResult("ok", Duration.ZERO, lastDayOfCalculation.toString());
-            }
-
-            LocalDate lastDayFromCsv = remoteCsvExtractor.retrieveLastDayInCsv();
-            if (lastDayOfCalculation.isBefore(lastDayFromCsv)) {
-                log.info("There are new data for graphs calculation, running the extraction right now.");
-                return createLatestGraphs();
-            } else {
-                log.warn("There are no data till now for the current day, wait for it and run manually or wait for the next run.");
-                return new GraphsGenerationResult("no", Duration.ZERO, null);
-            }
-
-        }
-    }
 
     public GraphsGenerationResult createLatestGraphs() throws IOException {
         GraphsGenerationResult nationalGraphs = createLatestNationalGraphs();
--- backend/src/main/java/it/minetti/scheduled/DataSchedulerService.java
+++ backend/src/main/java/it/minetti/scheduled/DataSchedulerService.java
@@ -1,12 +1,16 @@
 package it.minetti.scheduled;
 
 import it.minetti.data.DataRetrieverService;
+import it.minetti.images.LocalGraphsService;
+import it.minetti.images.r.CovidGraphsGenerator;
 import it.minetti.pcmdpc.RemoteCsvExtractor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -14,17 +18,25 @@ public class DataSchedulerService {
 
     private final RemoteCsvExtractor remoteCsvExtractor;
     private final DataRetrieverService dataRetrieverService;
+    private final LocalGraphsService localGraphsService;
+    private final CovidGraphsGenerator covidGraphsGenerator;
 
-    public DataSchedulerService(RemoteCsvExtractor remoteCsvExtractor, DataRetrieverService dataRetrieverService) {
+
+    public DataSchedulerService(RemoteCsvExtractor remoteCsvExtractor,
+                                DataRetrieverService dataRetrieverService,
+                                LocalGraphsService localGraphsService,
+                                CovidGraphsGenerator covidGraphsGenerator) {
         this.remoteCsvExtractor = remoteCsvExtractor;
         this.dataRetrieverService = dataRetrieverService;
+        this.localGraphsService = localGraphsService;
+        this.covidGraphsGenerator = covidGraphsGenerator;
     }
 
     @Scheduled(cron = "0 55 17 * * ?", zone = "Europe/Rome")
     @Scheduled(cron = "0 00 18 * * ?", zone = "Europe/Rome")
     @Scheduled(cron = "0 10 18 * * ?", zone = "Europe/Rome")
     @Scheduled(cron = "0 15 19 * * ?", zone = "Europe/Rome")
-    public void evictAndRetrieveIfNewData() { // TODO normalize with CovidGraphsGenerator.class
+    public void evictAndRetrieveDataIfNewData() {
         LocalDate lastDayOfData = dataRetrieverService.retrieveNationalData().getLastDate();
 
         if (lastDayOfData.isEqual(LocalDate.now())) {
@@ -43,4 +55,33 @@ public class DataSchedulerService {
         }
 
     }
+
+    @Scheduled(cron = "0 55 17 * * ?", zone = "Europe/Rome")
+    @Scheduled(cron = "0 00 18 * * ?", zone = "Europe/Rome")
+    @Scheduled(cron = "0 10 18 * * ?", zone = "Europe/Rome")
+    @Scheduled(cron = "0 15 19 * * ?", zone = "Europe/Rome")
+    public void generateImagesIfNewData() throws IOException {
+        Optional<LocalDate> optionalLastDayOfCalculation = localGraphsService.retrieveLatestCalculatedDay();
+        if (optionalLastDayOfCalculation.isEmpty()) {
+            log.warn("No graph has ever been calculated since now, seems it is the first time.");
+            covidGraphsGenerator.createLatestGraphs();
+        } else {
+            LocalDate lastDayOfCalculation = optionalLastDayOfCalculation.get();
+            if (lastDayOfCalculation.isEqual(LocalDate.now())) {
+                // the data for the day are published within the day
+                log.warn("No needs to create the new graphs since they are updated to {}.", lastDayOfCalculation);
+                return;
+            }
+
+            LocalDate lastDayFromCsv = remoteCsvExtractor.retrieveLastDayInCsv();
+            if (lastDayOfCalculation.isBefore(lastDayFromCsv)) {
+                log.info("There are new data for graphs calculation, running the extraction right now.");
+                covidGraphsGenerator.createLatestGraphs();
+            } else {
+                log.warn("There are no data till now for the current day, wait for it and run manually or wait for the next run.");
+            }
+
+        }
+    }
+
 }
