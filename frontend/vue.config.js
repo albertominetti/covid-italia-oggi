a16cce7ae4b8e1f56b9543758f1b28cba17e1163
Alberto Minetti <alberto.minetti@gmail.com>
2020-12-10 00:32:47 +0100

    new title and remote backend

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,24 +2,36 @@ module.exports = {
     // Change build paths to make them Maven compatible
     // see https://cli.vuejs.org/config/
     outputDir: 'dist',
+
     assetsDir: 'assets',
+
     devServer: {
         proxy: {
             '/api': {
                 //target: 'http://localhost:8080',
-                target: 'https://coviddi.ddns.net',
+                target: 'https://covid-sentry.ddns.net',
                 ws: true,
                 changeOrigin: true
             }
         },
         port: 8081,
     },
+
     chainWebpack: config => {
         config
             .plugin('html')
             .tap(args => {
-                args[0].title = 'Coviddi'
+                args[0].title = 'Covid Sentry'
                 return args
             })
+    },
+
+    pluginOptions: {
+      i18n: {
+        locale: 'it',
+        fallbackLocale: 'en',
+        localeDir: 'locales',
+        enableInSFC: false
+      }
     }
 }
