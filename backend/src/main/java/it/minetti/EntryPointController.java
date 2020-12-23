cd4a780e2708f4e1236192818b47ff52fd1e97db
Alberto Minetti <alberto.minetti@gmail.com>
2020-12-23 21:21:18 +0100

    fix additional endpoint forward

--- backend/src/main/java/it/minetti/EntryPointController.java
+++ backend/src/main/java/it/minetti/EntryPointController.java
@@ -10,8 +10,10 @@ public class EntryPointController {
 
     // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
     // Required because of 'mode: history' usage in frontend routing, see README for further details
-    @RequestMapping(value = "{_:^(?!index\\.html|api|covid-19.png).*$}")
-    public String redirectApi() {
+    @RequestMapping(value = {
+            "{_:^(?!index\\.html|api|covid-19.png|swagger-ui|actuator).*$}"
+    })
+    public String forward() {
         log.info("URL entered directly into the Browser, so we need to redirect...");
         return "forward:/";
     }
