8afdf5842e5fdc79e55d5bad4b3af4cb38867306
denial3x <alessandro.dieni95@gmail.com>
2020-12-23 22:57:11 +0100

    Implemented LocaleSwitcher component as a <b-form-select> VueBootstrap component

---
+++ frontend/src/components/LocaleSwitcher.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-form-select
+    v-model="$i18n.locale"
+    :options="supportedLocales"
+    class="text-large"
+    label-field="Lang"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { supportedLocales } from "@/i18n";
+
+@Component
+export default class LocaleSwitcher extends Vue {
+  get supportedLocales(): { text: string; value: string }[] {
+    return Object.entries(supportedLocales).map(locale => ({
+      value: locale[0],
+      text: locale[1]
+    }));
+  }
+}
+</script>
+
+<style scoped></style>
