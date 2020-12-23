0266846a0ca5bbdf3dd4e8e8b257ec61c48169af
Alberto Minetti <alberto.minetti@gmail.com>
2020-12-23 21:42:54 +0100

    select swagger exposed api

--- backend/src/main/java/it/minetti/config/SwaggerConfig.java
+++ backend/src/main/java/it/minetti/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package it.minetti.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -15,7 +16,9 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class).or(
+                        RequestHandlerSelectors.basePackage("org.springframework.boot.actuate"))
+                )
                 .paths(PathSelectors.any())
                 .build();
     }
